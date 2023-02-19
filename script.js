b5942f31254e1830aa4019759dfcdfc6bcc85f06
kiryauragan <you@example.comkirilltaranskyi@gmail.com>
2023-02-19 19:58:31 +0200

    script.js updated

---
+++ script.js
@@ -0,0 +1,22 @@
+// Запрос чисел
+let num1 = Number(prompt("Введите первое число:"));
+let num2 = Number(prompt("Введите второе число:"));
+
+// Сумма
+let sum = num1 + num2;
+
+// Разница
+let difference = num1 - num2;
+
+// Умножение
+let multiplication = num1 * num2;
+
+// Деление
+let divide = num1 / num2;
+
+// Алерт
+alert(`${num1} + ${num2} = ${sum}
+${num1} - ${num2} = ${difference}
+${num1} * ${num2} = ${multiplication}
+${num1} / ${num2} = ${divide}`
+);
